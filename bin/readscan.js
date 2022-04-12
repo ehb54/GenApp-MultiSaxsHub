e271da820f4f74a17e3a02d386085583c65b57d3
ehb54 <brookes@uthscsa.edu>
2022-04-12 12:37:31 -0500

    updates to readscan.js test program

--- bin/readscan.js
+++ bin/readscan.js
@@ -6,6 +6,11 @@ const scandata = require( "./scandata.js" );
 scandata.read( "testfile" );
 scandata.list();
 scandata.indextest();
-scandata.plot2d( [-1,-1,0] );
+// scandata.plot2d( [-1,-1,0] );
 
+console.log( scandata.csv() );
+
+// scandata.defaults( "multisaxshub" );
+console.log( JSON.stringify( scandata.contours( [ 'contrast_hydration', 'excluded_volume' ] ) ) );
+// console.log( JSON.stringify( scandata.contours( [ 'contrast_hydration', 'excluded_volume' ] ), null, 2 ) );
 
